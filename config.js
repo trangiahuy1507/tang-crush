8328ec097b9356c0d00756b01741414cee518f8a
trangiahuy1507 <89672337+trangiahuy1507@users.noreply.github.com>
2021-08-28 08:54:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'vậy cho em 1 cuộc hẹn hò khi gặp nhau nhé <3',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/g.hycute/
 }
